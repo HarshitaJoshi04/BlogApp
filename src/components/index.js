428fa4bac7570f1d4ff8c9fb8d12ef55003af797
HarshitaJoshi04 <hjoshikspuk04@gmail.com>
2026-03-01 21:47:18 +0530

    postForm

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,18 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from '../components/Header/Button'
+import Input from "../components/Input"
+import Select from "../components/Select"
+import RTE from "../components/RTE"
 export {
   Header,
   Footer,
   Container,
   Logo,
-  LogoutBtn
-  
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  RTE
 }
