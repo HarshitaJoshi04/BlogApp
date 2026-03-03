1f182b5fc45c874e5a1e532d09b773796a359961
HarshitaJoshi04 <hjoshikspuk04@gmail.com>
2026-03-03 19:00:45 +0530

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
-import Button from '../components/Header/Button'
+import Button from './Header/Button'
 import Input from "../components/Input"
 import Select from "../components/Select"
 import RTE from "../components/RTE"
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,11 @@
+//it is a configuration object
+//this object stores env variables so we can use safely throughout the app
 const conf = {
-  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),  //appwrite api url
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID), //unique id of appwrite project
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),//database id inside appwrite
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),//collection id isnide that db
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),// storage bucket id..used for file uploads like images
 };
 
 export default conf;
